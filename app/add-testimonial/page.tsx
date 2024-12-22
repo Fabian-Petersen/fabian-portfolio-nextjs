8c3a3f7ad723963f2eb5712acbfcf92c7eb6aa4c
Fabian <116721837+Fabian-Petersen@users.noreply.github.com>
2024-12-22 19:22:45 +0200

    complete testimonial section

---
+++ app/add-testimonial/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TestimonialForm from "@/components/testimonials/TestimonialForm";
+import BackHomeButton from "@/components/features/BackHomeButton";
+
+function AddTestimonial() {
+  return (
+    <main className="w-full grid bg-slate-200 dark:bg-bgDark py-32 h-screen">
+      <div className="w-full">
+        <div className="sm:px-[var(--all-pages-spacing)] px-[var(--all-pages-spacing-small)] md:max-w-6xl mx-auto grid grid-cols-1 place-content-center h-full">
+          <TestimonialForm />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default AddTestimonial;
