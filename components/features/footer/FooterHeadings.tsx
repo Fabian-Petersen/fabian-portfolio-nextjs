b8a9ad5e349e246fd8693759846d762bea84b809
Fabian <116721837+Fabian-Petersen@users.noreply.github.com>
2025-01-22 18:00:56 +0200

    changed footerHeading padding

--- components/features/footer/FooterHeadings.tsx
+++ components/features/footer/FooterHeadings.tsx
@@ -7,7 +7,7 @@ type Props = {
 const FooterHeadings = ({ heading }: Props) => {
   return (
     <h3
-      className={`text-clampFooterHeading font-bold tracking-wider ${heading === "Socials" ? "pb-0" : "pb-4"}`}
+      className={`text-clampFooterHeading font-bold tracking-wider ${heading === "Socials" ? "pb-0" : "pb-2"}`}
     >
       {heading}
     </h3>
